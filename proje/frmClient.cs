b182d95855d26a261988f71334cd6d22dce968d3
şimal <96944377+SimalGunduz@users.noreply.github.com>
2025-05-21 09:45:22 +0300

    Update frmClient.cs

--- proje/frmClient.cs
+++ proje/frmClient.cs
@@ -223,7 +223,8 @@ namespace proje
 
         private void metinŞifrelemeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+             frmMetinSifreleme sifreForm = new frmMetinSifreleme();
+             sifreForm.ShowDialog();
         }
     }
 }
